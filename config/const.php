3c44788ae558a72ed3f27e4f1b1a8b9b4df8fc8d
andilMc <68065104+andilMc@users.noreply.github.com>
2024-10-31 22:05:34 +0600

    Update const.php

--- config/const.php
+++ config/const.php
@@ -10,10 +10,4 @@ define("_HOST","localhost");
 define("_DB","bookme");
 define("_PORT","3306");
 
-// define("_USER","bookme_weredustat");
-// define("_PSWD","7421415b63d546129fe76800c0fd15614de1550a");
-// define("_HOST","2fw.h.filess.io");
-// define("_DB","bookme_weredustat");
-// define("_PORT","3307");
-
 
