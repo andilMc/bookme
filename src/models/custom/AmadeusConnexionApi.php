a2544db9a7128b086af1a9c6de4c9793c0b227d7
andilMc <68065104+andilMc@users.noreply.github.com>
2024-10-31 22:07:00 +0600

    Update AmadeusConnexionApi.php

--- src/models/custom/AmadeusConnexionApi.php
+++ src/models/custom/AmadeusConnexionApi.php
@@ -6,6 +6,6 @@ use Amadeus\Amadeus;
 class AmadeusConnexionApi
 {
     static function getConnexion() : Amadeus {
-        return Amadeus::builder("caOGGaQGllJ6njIOC5LWoBVF1FW2247i","dwD8P9ah3shVUwTO")->build();
+        return Amadeus::builder("########","##########")->build();
     }
 }
